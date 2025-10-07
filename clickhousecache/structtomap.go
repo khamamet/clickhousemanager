a18819afcdd99e137238fc6f9a378fed8a260834
Sergio <SG Computers>
2025-10-07 18:18:42 +0300

    map[]any Marshall fix

--- clickhousecache/structtomap.go
+++ clickhousecache/structtomap.go
@@ -1,55 +1,63 @@
 package clickhousecache
 
 import (
+	"encoding"
 	"reflect"
 	"time"
 )
 
-// Struct2Map converts struct to map.
-// result Key is:
-// db tag - if tag "db" is provided or
-// field name otherwise
 func Struct2Map(model interface{}) map[string]interface{} {
 	ret := make(map[string]interface{})
-
 	modelReflect := reflect.ValueOf(model)
-
 	if modelReflect.Kind() == reflect.Ptr {
 		modelReflect = modelReflect.Elem()
 	}
-
 	modelRefType := modelReflect.Type()
 	fieldsCount := modelReflect.NumField()
 
-	var fieldData interface{}
-
-	for i := range fieldsCount {
+	for i := 0; i < fieldsCount; i++ {
 		field := modelReflect.Field(i)
-
 		key := modelRefType.Field(i).Tag.Get("db")
 		if key == "-" {
-			// ignore
 			continue
 		}
+		if key == "" {
+			key = modelRefType.Field(i).Name
+		}
+		var fieldData interface{}
 
 		switch field.Kind() {
 		case reflect.Struct:
 			if t, ok := field.Interface().(time.Time); ok {
-				fieldData = t.Format("2006-01-02") // Format as string for ClickHouse
+				fieldData = t.Format("2006-01-02")
+			} else if t, ok := field.Interface().(encoding.TextMarshaler); ok {
+				b, err := t.MarshalText()
+				if err == nil {
+					fieldData = string(b)
+				} else {
+					fieldData = ""
+				}
 			} else {
+				// fallback: nested struct
 				fieldData = Struct2Map(field.Interface())
 			}
 		case reflect.Ptr:
-			fieldData = Struct2Map(field.Interface())
+			if field.IsNil() {
+				fieldData = nil
+			} else if t, ok := field.Interface().(encoding.TextMarshaler); ok {
+				b, err := t.MarshalText()
+				if err == nil {
+					fieldData = string(b)
+				} else {
+					fieldData = ""
+				}
+			} else {
+				fieldData = Struct2Map(field.Interface())
+			}
 		default:
 			fieldData = field.Interface()
 		}
-
-		if key == "" {
-			key = modelRefType.Field(i).Name
-		}
 		ret[key] = fieldData
 	}
-
 	return ret
 }
